0024999e7c1d4cd7528379d4f47b438756c67dc0
Benjamin Coutellier <bcoutellier@ext.sephora.fr>
2022-01-30 17:11:57 +0100

    :ambulance: cleaning testing secret

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -33,13 +33,15 @@ locals {
 }
 
 provider "google" {
-  project = var.gcp_project_id
-  region  = local.gcp_region
+  credentials = file("terraform-deploy.json")
+  project     = var.gcp_project_id
+  region      = local.gcp_region
 }
 
 provider "google-beta" {
-  project = var.gcp_project_id
-  region  = local.gcp_region
+  project     = var.gcp_project_id
+  region      = local.gcp_region
+  credentials = file("terraform-deploy.json")
 }
 
 provider "kubernetes" {

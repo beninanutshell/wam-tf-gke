841ff8ed9e2a6fce1d16bb760faa71584b78d914
Benjamin Coutellier <bcoutellier@ext.sephora.fr>
2022-04-10 19:22:17 +0200

    :art: Make gke a little bit more sexy

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.0.0"
+      version = ">= 4.16.0"
     }
     google-beta = {
       source  = "hashicorp/google"
-      version = ">= 4.0.0"
+      version = ">= 4.16.0"
     }
     random = {
       source  = "hashicorp/random"

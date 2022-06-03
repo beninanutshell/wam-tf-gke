c45222aac5d617cc2058b001293664e8ceefbdf9
COUTELLIER Benjamin <benjamin.coutellier@gmail.com>
2022-06-03 18:07:02 -0400

    changing terraform variables values

--- env/bootstrap/backend.tf
+++ env/bootstrap/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket      = "it-demo-app-tfstate-9c03"
     prefix      = "terraform/wam-tf-gke/state/bootstrap/"
-    #credentials = "terraform-deploy.json"
+    credentials = "terraform-deploy.json"
   }
 }
--- modules/gke/address.tf
+++
@@ -1,4 +0,0 @@
-resource "google_compute_address" "load_balancer_ip" {
-  name         = "lb-external-ip"
-  address_type = "EXTERNAL"
-}

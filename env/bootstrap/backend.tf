1ba1dc696c580fd9dd17d0f395a7afbe0d19ba76
Benjamin Coutellier <bcoutellier@sephora.fr>
2023-02-16 16:07:11 -0500

    :bricks: Fic bkt ID for tfstate

--- env/bootstrap/backend.tf
+++ env/bootstrap/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket      = "devops-lab-app-build-state-wam-devops-c-iac-pipeline-pt78"
+    bucket      = "devops-lab-app-build-state-wam-devops-c-iac-pipeline-5hm7"
     prefix      = "terraform/app/devops/wam-tf-gke/state/env/bootstrap/"
     credentials = "terraform-deploy.json"
   }
